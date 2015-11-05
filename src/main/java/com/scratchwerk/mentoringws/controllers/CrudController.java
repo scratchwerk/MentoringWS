b015edd41bf73b7075c9068601d62d654d017a06
scratchwerk <ronnie.king@gmail.com>
2015-11-05 06:50:15 -0500

    Adding Mentee paths to CRUD

--- src/main/java/com/scratchwerk/mentoringws/controllers/CrudController.java
+++ src/main/java/com/scratchwerk/mentoringws/controllers/CrudController.java
@@ -11,14 +11,46 @@ import com.scratchwerk.mentoringws.models.Mentor;
 @RequestMapping("/crud")
 public class CrudController {
 
+	//Mentor Paths
 	@RequestMapping(value="/mentor", method=RequestMethod.GET)
 	public Mentor retrieveMentor(@RequestParam(value="id", defaultValue="0") int id) {
         return new Mentor(id, "Ronnie", "King");
     }
 	
-	@RequestMapping(value="/mentor", method=RequestMethod.PUT)
+	@RequestMapping(value="/mentor", method=RequestMethod.POST)
 	public String createMentor(@RequestParam(value="id", defaultValue="0") int id) {
         return "You just created a new Mentor";
+    }
+	
+	@RequestMapping(value="/mentor", method=RequestMethod.PUT)
+	public String updateMentor(@RequestParam(value="id", defaultValue="0") int id) {
+        return "You just updated a new Mentor";
+    }
+	
+	@RequestMapping(value="/mentor", method=RequestMethod.DELETE)
+	public String deleteMentor(@RequestParam(value="id", defaultValue="0") int id) {
+        return "You just deleted a new Mentor";
+    }
+	
+	//Mentee Paths
+	@RequestMapping(value="/mentee", method=RequestMethod.GET)
+	public Mentor retrieveMentee(@RequestParam(value="id", defaultValue="0") int id) {
+        return new Mentor(id, "Ronnie", "King");
+    }
+	
+	@RequestMapping(value="/mentee", method=RequestMethod.POST)
+	public String createMentee(@RequestParam(value="id", defaultValue="0") int id) {
+        return "You just created a new Mentee";
+    }
+	
+	@RequestMapping(value="/mentee", method=RequestMethod.PUT)
+	public String updateMentee(@RequestParam(value="id", defaultValue="0") int id) {
+        return "You just updated a new Mentee";
+    }
+	
+	@RequestMapping(value="/mentee", method=RequestMethod.DELETE)
+	public String deleteMentee(@RequestParam(value="id", defaultValue="0") int id) {
+        return "You just deleted a new Menteer";
     }
 	
 }
