e21db1c19655aa2a0659a52447c5495015c76f44
scratchwerk <ronnie.king@gmail.com>
2015-11-04 19:38:19 -0500

    Adding gitignore files

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application{
 
+	//This is where it starts
 	public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
